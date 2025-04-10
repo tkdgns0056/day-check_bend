af9029d2c59110053a38cf374d359fcb27040b9b
tkdgns0056 <tkdgns0056@gmail.com>
2025-04-11 08:24:47 +0900

    로그인 이슈

--- src/main/java/com/project/daycheck/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/project/daycheck/config/security/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
@@ -12,6 +13,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -51,7 +53,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private String resolverToken(HttpServletRequest request) {
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
 
+        // 토큰 로그 추가
+        log.debug("Authorization 헤더: {}", bearerToken);
+
         if(StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)){
+            String token = bearerToken.substring(7); // "Bearer 제거"
+            log.debug("추출된 토큰: {}", token);
             return bearerToken.substring(7); // "Bearer " 제거
         }
 
